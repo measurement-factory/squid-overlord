caf043ebf7a685d18be6268b41c96d3b409362b5
Alex Rousskov <rousskov@measurement-factory.com>
2019-03-11 14:29:27 -0600

    Switched to a simpler, easier-to-distribute/install Perl-based code
    Without node v8+ or Babel, Javascript development quickly gets ugly.
    Installing node is already a hassle/risk, and installing modern node is
    even more difficult, especially in environments with old node installed.
    The overlord script itself is supposed to be simple -- it does not
    really need most benefits of "parallel" [I/O] threads that node brings
    to the table.
    
    The original plan was to encapsulate node installation complexities in a
    docker service, but I realized that it would be difficult to control a
    Squid instance from a [separate] docker. Accessing Squid files would
    have been easy, but starting/stopping processes outside the controlling
    docker would be very tricky!
    
    If netcat did not drop its -c option, this Overlord script could have
    been written in shell to improve its portability further.

--- README.md
+++ README.md
@@ -3,52 +3,15 @@
 Squid Overlord controls a Squid service, including its configuration,
 lifetime, and logs interrogation. It facilitates black-box functionality
 testing by isolating generic HTTP proxy test case logic from Squid
-manipulation specifics. This overlord is a node.js script and a web service
+manipulation specifics. This overlord is a Perl script and a web service
 with RESTish API.
 
-## Invocation
+## Typical invocation
 
-    squid-overlord.js /usr/local/squid/
+    sudo -u nobody ./overlord.pl
 
-The only parameter is the Squid installation directory.
-
-
-## Commands
+## Supported commands
 
 *Method* | *URI* | *Description*
 --- | --- | ---
-GET | /version | Returns Squid version. Does not require a running Squid instance. Not yet supported.
-POST | /start | Starts a new Squid instance. Returns when the instance is accepting requests. Currently, at most one concurrent instance is supported.
-POST | /stop | Stops a running Squid instance. Returns when the instance is no longer running.
-POST | /reconfigure | Reconfigures a running Squid instance. Returns when the instance resumes accepting requests.
-POST | /reset | Shuts down Squid if it is running. Starts Squid. Not yet supported.
-
-## Squid Configuration blocks
-
-Squid configuration is specified as a set of requirements. Each requirement
-describes some aspect of Squid behavior or functionality. Overlord translates
-these high-level declarations into actual `squid.conf` statements, adding the
-missing pieces. Relying on defaults, Overlord attempts to generate the
-simplest configuration that can still satisfy the requirements. Since defaults
-may change, clients should only specify the requirements they actually care
-about.
-
-Requirement types are documented further below, but here is a configuration requirements example:
-
-```
-{
-  "version": 1,
-  "configuration": [
-    "caching": "false",
-    "dns_resolvers": [ "127.0.0.1" ],
-  ]
-}
-```
-
-*Requirement* | *Type* | *Description*
---- | --- | ---
-caching | boolean | Whether Squid should cache. By default, memory caching is used (because it is simpler to enable).
-workers | integer | The number of SMP workers.
-dns-resolvers | array of strings | Use the DNS resolver(s) running at the specified IP address(es).
-
-Configuration is transmitted as a JSON-encoded HTTP request body.
+POST | /reset | Shuts down Squid if it is running. Starts Squid with the configuration file created from the POST request body.
---
+++ overlord.pl
@@ -0,0 +1,225 @@
+#!/usr/bin/perl -w
+
+# Implements the server side of the Proxy Overlord Protocol:
+# HTTP/1 POST requests for "/reset" URLs are translated into
+# a Squid (re)start with the configuration from the POST body.
+#
+# This Overlord only supports basic HTTP/1 syntax and message
+# composition rules to avoid being dependent on non-Core modules.
+
+
+# These Core modules should be available in nearly all Perl installations.
+use Carp;
+use IO::Socket;
+use IO::File;
+use Getopt::Long;
+use strict;
+use warnings;
+use English;
+
+my $ListeningPort = 13128;
+my $Prefix = "/usr/local/squid";
+my $Help = 0;
+
+GetOptions(
+    "port=i" => \$ListeningPort,
+    "prefix=s" => \$Prefix,
+) or die("usage: $0 [--listen <port>] [--prefix <Squid installation prefix>]\n");
+
+chdir($Prefix) or die("Cannot set working directory to $Prefix: $?\n");
+
+my $SquidPidFilename = "$Prefix/var/run/squid.pid";
+my $SquidConfigFilename = "$Prefix/etc/squid-overlord.conf"; # maintained by us
+my $SquidExeFilename = "$Prefix/sbin/squid";
+my $SquidListeningPort = 3128;
+
+# (re)start Squid form scratch with the given configuration
+sub resetSquid
+{
+    my $config = shift;
+    # warn("Resetting to:\n$config\n");
+
+    &stopSquid() if &squidIsRunning();
+    &writeSquidConfiguration($config);
+    &startSquid();
+}
+
+sub stopSquid
+{
+    &shutdownSquid() if &squidIsRunning();
+    warn("Squid is not running\n");
+}
+
+sub shutdownSquid
+{
+    my $pid = &squidPid();
+    kill('SIGZERO', $pid) == 1 or die("cannot signal Squid ($pid): $EXTENDED_OS_ERROR\n");
+    warn("shutting Squid ($pid) down...\n");
+    kill('SIGINT', $pid) or return;
+
+    &waitFor("deleted $SquidPidFilename", sub { ! &squidIsRunning() });
+}
+
+sub killSquid
+{
+    my $pid = &squidPid();
+    kill('SIGZERO', $pid) == 1 or die("cannot signal Squid ($pid): $EXTENDED_OS_ERROR\n");
+    warn("killing Squid ($pid) process...\n");
+    kill('SIGKILL', $pid) == 1 or return;
+    die("cannot kill Squid: $EXTENDED_OS_ERROR\n") if kill('SIGZERO', $pid) == 1;
+}
+
+sub writeSquidConfiguration
+{
+    my $config = shift;
+    my $out = IO::File->new("> $SquidConfigFilename")
+        or die("cannot create $SquidConfigFilename: $!\n");
+    $out->print($config) or die("cannot write $SquidConfigFilename: $!\n");
+    $out->close() or die("cannot finalize $SquidConfigFilename: $!\n");
+    warn("created ", length($config), "-byte $SquidConfigFilename\n");
+}
+
+sub startSquid
+{
+    my $cmd = "$SquidExeFilename";
+    $cmd .= " -C "; # prefer "raw" errors
+    $cmd .= " -f $SquidConfigFilename";
+    $cmd .= " > var/logs/squid.out 2>&1";
+    warn("running: $cmd\n");
+    system($cmd) == 0 or die("cannot start Squid: $!\n");
+
+    &waitFor("running Squid", \&squidIsRunning);
+    &waitFor("listening Squid", \&squidIsListening);
+    warn("Squid is running\n");
+}
+
+sub waitFor
+{
+    my ($description, $goalFunction) = @_;
+
+    for (my $iterations = 0; !&{$goalFunction}; ++$iterations) {
+        warn("waiting for $description\n") if $iterations % 60 == 0;
+        sleep(1);
+    }
+}
+
+sub squidIsRunning() {
+    # assume not running because there is no PID
+    return 0 unless -e $SquidPidFilename;
+
+    my $pid = &squidPid();
+    my $killed = kill('SIGZERO', $pid);
+    $killed = -1 unless defined $killed;
+
+    # clearly running
+    return 1 if $killed == 1;
+
+    if ($killed == 0 && $!{ESRCH}) {
+        warn("assuming Squid ($pid) has died");
+        return 0;
+    }
+
+    warn("assume Squid ($pid) is running: $EXTENDED_OS_ERROR\n");
+    return 1;
+}
+
+sub squidIsListening() {
+    # TODO: Check that lsof works at all: -p $$
+
+    # We do not specify the IP address part because
+    # lsof -i@127.0.0.1 fails when Squid is listening on [::].
+    # Should we configure Squid to listen on a special-to-us ipv4-only port?
+    my $lsof = "lsof -Fn -w -i:$SquidListeningPort";
+    if (system("$lsof > /dev/null 2>&1") == 0) {
+        #warn("somebody is listening on port $SquidListeningPort\n");
+        return 1;
+    } else {
+        #warn("nobody listens on port $SquidListeningPort\n");
+        system($lsof); # will show usage error/problem if any
+        return 0;
+    }
+}
+
+sub squidPid
+{
+    my $pid = `cat $SquidPidFilename` or die("cannot determine Squid PID: $@\n");
+    chomp($pid);
+    return $pid;
+}
+
+
+# "parse" the client request and pass the details to the command-processing sub
+sub handleClient
+{
+    my $client = shift;
+
+    my $header = '';
+    while (<$client>) {
+        last if /^\s*$/;
+        $header .= $_;
+    }
+
+    if ($header =~ m@^POST\s+/reset\s@s &&
+        $header =~ m@^Content-Length:\s*(\d+)@im) {
+        &resetSquid(&receiveBody($client, $1));
+        &sendResponse($client, "200 OK", "");
+        return;
+    }
+
+    die("unsupported Proxy Overlord Protocol request:\n$header\nstopped");
+}
+
+sub receiveBody
+{
+    my ($client, $bodyLength) = @_;
+
+    my $body;
+    my $result = $client->read($body, $bodyLength);
+    die("cannot receive request body: $@") unless defined $result;
+    die("received truncated request body: ",
+        length $body, " vs. the expected $bodyLength bytes\n")
+        if length $body != $bodyLength;
+    return $body;
+}
+
+sub writeError
+{
+    my ($client, $error) = @_;
+    warn("Error: $error\n");
+    return &sendResponse($client, "555 External Server Error", $error);
+}
+
+sub sendResponse
+{
+    my ($client, $status, $body) = @_;
+
+    warn("responding with $status\n");
+
+    my $response = '';
+    $response .= "HTTP/1.1 $status\r\n";
+    $response .= "Content-Length: " . (length $body) . "\r\n";
+    $response .= "\r\n";
+    $response .= $body;
+
+    my $result = $client->send($response)
+        or die("failed to write a $status response: $@\n");
+    die("wrote truncated $status response") if $result != length $response;
+}
+
+my $server = IO::Socket::INET->new(
+    LocalPort => $ListeningPort,
+    Type      => SOCK_STREAM,
+    Reuse     => 1,
+    Listen    => 10, # SOMAXCONN
+) or die("Cannot listen on on TCP port $ListeningPort: $@\n");
+
+warn("$0 listens on TCP port $ListeningPort\n");
+
+while (my $client = $server->accept()) {
+    eval { &handleClient($client); };
+    my $error = $@;
+    eval { &writeError($client, $error) } if $error; # but swallow cascading errors
+    close($client) or warn("cannot close client connection: $@\n");
+}
+
+exit(0);
