c9946f4d183908b5f2dccb5e52a16607f8830c98
Alex Rousskov <rousskov@measurement-factory.com>
2022-10-23 22:04:03 -0400

    Support POP v5 and its kids-expected option for /reset and /restart
    Use the new option to wait until all Squid kid processes register with
    Coordinator before considering Squid fully operational. Without this
    wait, tests like busy-restart were failing on GitHub, probably because a
    kid could get its listening port and receive the test transaction before
    it got the disker. Now the chance of that happening is much smaller.

--- overlord.pl
+++ overlord.pl
@@ -16,9 +16,10 @@ use Getopt::Long;
 use strict;
 use warnings;
 use English;
-# These are in Perl Core since Perl v5.14: corelist -v 5.14.0
-use JSON::PP;
+# These are in Perl Core since Perl v5.14 (or earlier; see corelist -v 5.14.0)
+use Data::Dumper;
 use HTTP::Tiny;
+use JSON::PP;
 
 
 my $MyListeningPort = 13128;
@@ -38,13 +39,13 @@ my $SquidLogsDirname = "$SquidPrefix/var/logs/overlord";
 my $SquidCachesDirname = "$SquidPrefix/var/cache/overlord";
 my $SquidOutFilename = "$SquidLogsDirname/squid.out";
 
-my $SupportedPopVersion = '4';
+my $SupportedPopVersion = '5';
 
 # Names of all supported POP request options (updated below).
 # There is also 'config_' but that "internal" option is added by us.
 my @SupportedOptionNames = ();
 
-# /start option
+# /reset option
 my $onameListeningPorts = 'listening-ports';
 push @SupportedOptionNames, $onameListeningPorts;
 
@@ -58,6 +59,10 @@ my %SignalsByShutdownManner = (
 );
 my $OptStopDefault = 'immediately';
 die() unless exists $SignalsByShutdownManner{$OptStopDefault};
+
+# /reset and /restart option
+my $onameKidsExpected = 'kids-expected';
+push @SupportedOptionNames, $onameKidsExpected;
 
 my %SupportedOptionNameIndex = map { $_ => 1 } @SupportedOptionNames;
 
@@ -79,6 +84,7 @@ sub resetSquid
 {
     my ($options) = @_;
     my $config = $options->{config_};
+    warn("Resetting Squid\n");
     # warn("Resetting to:\n$config\n");
 
     &stopSquid($options) if &squidIsRunning();
@@ -160,7 +166,8 @@ sub startSquidInBackground
     &startSquid_();
     &waitFor("running Squid", \&squidIsRunning);
     &waitFor("listening Squid", sub { return &squidIsListeningOnAllPorts($options); });
-    warn("Squid is listening\n");
+    &waitFor("Squid with all kids registered", sub { return &squidHasAllKids($options); });
+    warn("Squid is running and ready\n");
 }
 
 sub runSquidInForeground
@@ -260,9 +267,7 @@ sub squidIsListeningOnAllPorts() {
 
     # We do not parse http_port lines because they may have ${process_number}
     # and/or may be affected by macros.
-    my $pname = 'listening-ports';
-    die("missing $pname in ", join(",", keys %{$options})) unless defined $options->{$pname};
-    my (@ports) = split(/\s*,\s*/, $options->{$pname});
+    my (@ports) = split(/\s*,\s*/, &requiredOption($onameListeningPorts, $options));
     die("cannot determine Squid listening ports") unless @ports;
 
     return &squidIsListeningOn(@ports);
@@ -284,6 +289,13 @@ sub squidIsListeningOn() {
         return 0 unless defined $firstMatch;
     }
     return 1;
+}
+
+sub requiredOption() {
+    my ($oname, $options) = @_;
+    my $result = $options->{$oname};
+    die("missing $oname in ", join(",", keys %{$options})) unless defined $result;
+    return $result;
 }
 
 sub squidPid
@@ -333,16 +345,51 @@ sub finishCaching
 # whether Squid has StoreEntries in SWAPOUT_WRITING state
 sub squidHasSwapouts
 {
-    # XXX: Require listening-ports
-    my $url = 'http://127.0.0.1:3128/squid-internal-mgr/openfd_objects';
+    my $mgrPage = &getCacheManagerResponse('openfd_objects')->{content};
+    return $mgrPage =~ /SWAPOUT_WRITING/;
+}
+
+# whether all Squid kid processes have registered with Coordinator
+# assumes that Squid is running and listening
+sub squidHasAllKids
+{
+    my ($options) = @_;
+
+    my $kidsExpected = &requiredOption($onameKidsExpected, $options);
+
+    # Any mgr action that reports per-kid statistics for all kids would work.
+    # TODO: If there is a disker, we should (also) wait for kids to find the
+    # disker strand (mtFindStrand). Is there a mgr page reflecting that state?
+    my $mgrPage = &getCacheManagerResponse('openfd_objects')->{content};
+    # how many kids completed their by kidN {...} by kidN reports
+    my $kidsRegistered = () = $mgrPage =~ /^[}] by kid\d+/mg;
+
+    return 1 if !$kidsExpected && !$kidsRegistered; # no-SMP
+
+    # Coordinator is not explicitly visible in cache manager output, but
+    # without it, there would be no successful SMP cache manager output.
+    die("unexpected kids: $kidsRegistered >= $kidsExpected; stopped")
+        if $kidsRegistered >= $kidsExpected;
+
+    return ($kidsRegistered + 1 == $kidsExpected) ? 1 : 0;
+}
+
+# successful Http::Tiny response object for a given mgr:page ID
+sub getCacheManagerResponse
+{
+    my ($pageId) = @_;
+    die() unless defined $pageId;
+
+    my $url = "http://127.0.0.1:3128/squid-internal-mgr/$pageId";
     my $response = HTTP::Tiny->new->get($url);
     die("Cache manager request failure:\n" .
         "Request URL: $url\n" .
         "Response status: $response->{status} ($response->{reason})\n" .
+        Dumper($response->{headers}) . "\n" .
         $response->{content} . "\nnear")
         unless $response->{success} && $response->{status} == 200;
 
-    return $response->{content} =~ /SWAPOUT_WRITING/;
+    return $response;
 }
 
 sub parseOptions
