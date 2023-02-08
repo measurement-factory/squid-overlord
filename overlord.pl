measurement-factory/squid-overlord

commit 4df876bb8ab1c79bb1b22d150f24d01d1417cbb5

Support POP v7 that reports individual DUT problems using an array
... instead of merging all of them into one string/blob

overlord.pl
@@ -39,7 +39,7 @@ my $SquidLogsDirname = "$SquidPrefix/var/logs/overlord";
 my $SquidCachesDirname = "$SquidPrefix/var/cache/overlord";
 my $SquidOutFilename = "$SquidLogsDirname/squid.out";
 
-my $SupportedPopVersion = '6';
+my $SupportedPopVersion = '7';
 
 # Names of all supported POP request options (updated below).
 # There is also 'config_' but that "internal" option is added by us.
@@ -110,8 +110,9 @@ sub checkSquid
 {
     my $report = {};
 
-    $report->{problems} = `egrep -am10 '^[0-9./: ]+ kid[0-9]+[|] (WARNING|ERROR|assertion)' $SquidLogsDirname/cache-*.log 2>&1`;
-    $report->{problems} = '' unless $report->{problems} =~ /\S/;
+    my $problems = `egrep -am10 '^[0-9./: ]+ kid[0-9]+[|] (WARNING|ERROR|assertion)' $SquidLogsDirname/cache-*.log 2>&1`;
+    # split into individual problems, removing the trailing LF from each problem
+    $report->{problems} = [ split(/\n$|\n[^\s]/s, $problems) ];
 
     my $xactLogged = `cat $SquidLogsDirname/access-*.log | wc -l`;
     $xactLogged =~ s/^\s+|\s+$//sg; # remove leading and trailing whitespace

commit f37853dc16e3064cb9dcf3272c3ab2c3b17afe04

fixup: Do not eat the first character of subsequent problem lines
Broken in the previous commit 4df876b.

overlord.pl
@@ -112,7 +112,7 @@ sub checkSquid
 
     my $problems = `egrep -am10 '^[0-9./: ]+ kid[0-9]+[|] (WARNING|ERROR|assertion)' $SquidLogsDirname/cache-*.log 2>&1`;
     # split into individual problems, removing the trailing LF from each problem
-    $report->{problems} = [ split(/\n$|\n[^\s]/s, $problems) ];
+    $report->{problems} = [ split(/\n$|\n(?!\s)/s, $problems) ];
 
     my $xactLogged = `cat $SquidLogsDirname/access-*.log | wc -l`;
     $xactLogged =~ s/^\s+|\s+$//sg; # remove leading and trailing whitespace